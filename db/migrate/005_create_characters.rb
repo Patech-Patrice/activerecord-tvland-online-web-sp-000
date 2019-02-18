534c38d31f789e2e2993e755d2cafc7215cd6bb0
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-02-18 19:17:43 +0000

    Automatically backed up by Learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,10 +1,10 @@
-class CreateCharacters < ActiveRecord::Migration[4.2]
+class CreateCharacters < ActiveRecord::Migration[5.1]
   def change
     create_table :characters do |t|
       t.string :name
       t.integer :show_id
-      t.integer :actor_id
-      t.string  :catchphrase
+      #t.integer :actor_id
+      #t.string  :catchphrase
     end
   end
 end
